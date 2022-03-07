f5fd5e47cc8fcfab475b86abb2b3ede5a88ad25d
tasawarahmed <49127897+tasawarahmed@users.noreply.github.com>
2022-03-07 13:33:44 +0500

    Property interface added

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../iProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -6,52 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-  Properties: Array<any> = [
-    {
-      Id: 1,
-      Name: 'Birla House',
-      Type: 'House',
-      Price: 120000,
-    },
-    {
-      Id: 2,
-      Name: 'Shazia House',
-      Type: '1BHK',
-      Price: 240000,
-    },
-    {
-      Id: 3,
-      Name: 'Tasawar House',
-      Type: '2BHK',
-      Price: 360000,
-    },
-    {
-      Id: 4,
-      Name: 'Jazib House',
-      Type: '3BHK',
-      Price: 480000,
-    },
-    {
-      Id: 5,
-      Name: 'Ahmed House',
-      Type: '4BHK',
-      Price: 600000,
-    },
-    {
-      Id: 6,
-      Name: 'Zuha House',
-      Type: '5BHK',
-      Price: 720000,
-    },
-    {
-      Id: 7,
-      Name: 'Aunt House',
-      Type: '6BHK',
-      Price: 840000,
+  Properties: Array<IProperty> = [];
+
+  constructor(private housingService:HousingService) {}
+
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(data => {
+      this.Properties = data;
+      console.log(data);
+    }, error => {
+      console.log(error);
     }
-  ];
-
-  constructor() {}
-
-  ngOnInit(): void {}
+    );
+  }
 }
