4ffd83ae211993d6206b554bf29c54807ea1d446
tasawarahmed <49127897+tasawarahmed@users.noreply.github.com>
2022-03-07 09:37:57 +0500

    Initial Commit

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent implements OnInit {
+
+  Property: any = {
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price": 120000
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
